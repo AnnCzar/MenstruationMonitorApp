f73d349a55f361a030bc431abee8f4f9418d657c
AnnCzar <268319@student.pwr.edu.pl>
2024-04-02 21:53:42 +0200

    adding windows+activities

---
+++ app/src/main/java/com/example/project/MainWindowPeriodActivity.kt
@@ -0,0 +1,17 @@
+package com.example.project
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainWindowPeriodActivity : AppCompatActivity(){
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_window_period)
+
+
+    }
+
+
+}
