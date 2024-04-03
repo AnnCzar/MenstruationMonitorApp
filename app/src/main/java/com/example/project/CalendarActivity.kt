1d6458acf0662582086a30bd2cb9472eb551b192
AgnBrd <268339@student.pwr.edu.pl>
2024-04-04 00:46:41 +0200

    Adding viewa transitions

--- app/src/main/java/com/example/project/CalendarActivity.kt
+++ app/src/main/java/com/example/project/CalendarActivity.kt
@@ -1,16 +1,20 @@
 package com.example.project
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class CalendarActivity : AppCompatActivity(){
 
-
+    private lateinit var daysToEndOfMenstruaction: EditText
+    private lateinit var button_reg: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.calendar)
 
-
+        daysToEndOfMenstruaction = findViewById(R.id.daysToEndOfMenstruaction)
+//        button_reg = findViewById(R.id.button_register)
     }
 
 
