b8a5dd1f8b6ec67f18b79f849fdd2b88e73626a8
AgnBrd <268339@student.pwr.edu.pl>
2024-04-17 11:42:48 +0200

    x

---
+++ app/src/main/java/com/example/project/CalendarActivity-pc-aga.kt
@@ -0,0 +1,31 @@
+package com.example.project
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.CalendarView
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+class CalendarActivity : AppCompatActivity(){
+
+    private lateinit var daysToEndOfMenstruaction: EditText
+    private lateinit var calendar: CalendarView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.calendar)
+
+        daysToEndOfMenstruaction = findViewById(R.id.daysToEndOfMenstruaction)
+        calendar = findViewById(R.id.calendarView)
+
+        calendar.setOnDateChangeListener {_, year, month, day ->
+            val date = ("%02d".format(day) + "-" + "%02d".format(month+1) + "%02d".format(year))
+            openDayPeriodActivity()
+        }
+    }
+    private fun openDayPeriodActivity() {
+        val intent = Intent(this, DayPeriodActivity::class.java)
+        startActivity(intent)
+    }
+
+}
--- app/src/main/java/com/example/project/CalendarActivity.kt
+++ app/src/main/java/com/example/project/CalendarActivity.kt
@@ -1,31 +1,21 @@
 package com.example.project
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.CalendarView
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class CalendarActivity : AppCompatActivity(){
 
     private lateinit var daysToEndOfMenstruaction: EditText
-    private lateinit var calendar: CalendarView
+    private lateinit var button_reg: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.calendar)
 
         daysToEndOfMenstruaction = findViewById(R.id.daysToEndOfMenstruaction)
-        calendar = findViewById(R.id.calendarView)
+//        button_reg = findViewById(R.id.button_register)
+    }
 
-        calendar.setOnDateChangeListener {_, year, month, day ->
-            val date = ("%02d".format(day) + "-" + "%02d".format(month+1) + "%02d".format(year))
-            openDayPeriodActivity()
-        }
-    }
-    private fun openDayPeriodActivity() {
-        val intent = Intent(this, DayPeriodActivity::class.java)
-        startActivity(intent)
-    }
 
 }
