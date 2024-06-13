20bd16b93bc6ba146f0d633d054238dd7d3d2525
AnnCzar <268319@student.pwr.edu.pl>
2024-06-13 11:12:14 +0200

    added drinks count

--- app/src/main/java/com/example/project/MainWindowPeriodActivity.kt
+++ app/src/main/java/com/example/project/MainWindowPeriodActivity.kt
@@ -299,19 +299,7 @@ class MainWindowPeriodActivity : AppCompatActivity() {
             }
     }
 
-//    private fun createNotificationChannel() {
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//            val name = getString(R.string.channel_name)
-//            val descriptionText = getString(R.string.channel_description)
-//            val importance = NotificationManager.IMPORTANCE_DEFAULT
-//            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
-//                description = descriptionText
-//            }
-//            val notificationManager: NotificationManager =
-//                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-//            notificationManager.createNotificationChannel(channel)
-//        }
-//    }
+
 private fun scheduleNotification() {
     val intent = Intent(this, MedicineReminderReceiver::class.java)
     val pendingIntent = PendingIntent.getBroadcast(
